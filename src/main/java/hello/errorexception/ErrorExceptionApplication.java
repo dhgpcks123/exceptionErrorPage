9b03734c1d5b5dbbc17f2739f6e32401d5310372
hyechan <dhgpcks123@naver.com>
2021-08-12 20:46:31 +0900

    git init

---
+++ src/main/java/hello/errorexception/ErrorExceptionApplication.java
@@ -0,0 +1,13 @@
+package hello.errorexception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrorExceptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/errorexception/ErrorExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.errorexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
