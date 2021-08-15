364311f24950330416766104adfb507483ad99f5
hyechan <dhgpcks123@naver.com>
2021-08-15 17:21:10 +0900

    interceptor 오류페이지 요청 시 처리

--- src/main/java/hello/errorexception/WebConfig.java
+++ src/main/java/hello/errorexception/WebConfig.java
@@ -1,9 +1,12 @@
 package hello.errorexception;
 
 import hello.errorexception.filter.LogFilter;
+import hello.errorexception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -14,7 +17,18 @@ import javax.servlet.annotation.WebFilter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico","/error", "/error-page/**"); //오류 페이지 경로
+//                .dispatcherType 이런 거 아예 없어.
+                // excludePathPatterns에 담으면 interceptor 안 타고 지나감.
+
+    }
+
+//    @Bean
     public FilterRegistrationBean logFilter(){
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
@@ -22,7 +36,7 @@ public class WebConfig implements WebMvcConfigurer {
         filterFilterRegistrationBean.addUrlPatterns("/*");
 //        filterFilterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
         //이 필터는 request, error이 경우 호출됩니다.
-        // default는 Request만.
+        // default는 Request만. = request만 필터를 타고 내려온다는 의미겠지!
         return filterFilterRegistrationBean;
     }
 }
